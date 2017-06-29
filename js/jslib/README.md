94ceb7f6467e37943c98536f7c8927c9893b6ec6
NxtChg <admin@nxtchg.com>
2017-06-29 21:34:41 +0300

    "jslib" started.

---
+++ js/jslib/README.md
@@ -0,0 +1,12 @@
+
+# Javascript helpers
+
+Helper libraries, like jQuery, are usually bloated and there is no easy way to select only individual parts you need.
+
+This is a collection of small, stand-alone pieces you can easily combine into a single JS file with the _merge.bat_ script.
+
+They all use a global **"js"** root object as a namespace, for example `js.get_cookie()`.
+
+The code is usually so simple that you don't even need any additional documentation.
+
+Feel free to submit your own pieces, provided they are small and beautiful :)
---
+++ js/jslib/cookies.js
@@ -0,0 +1,35 @@
+/*=============================================================================
+  Created by NxtChg (admin@nxtchg.com), 2017. License: Public Domain.
+=============================================================================*/
+
+var js = js || {};
+//_____________________________________________________________________________
+
+js.get_cookie = function(name)
+{
+	var arr = document.cookie.split(';');
+
+	for(var i = 0; i < arr.length; i++)
+	{
+		var c = arr[i], p = -1; while(c.charCodeAt(++p) < 33);
+		
+		if(c.indexOf(name+'=', p) == p) return c.substr(p + name.length + 1);
+	}
+};//___________________________________________________________________________
+
+js.set_cookie = function(name, val, days, path) // (name, "", -1) to delete a cookie
+{
+	var enc = encodeURIComponent, v = enc(val);
+	
+	if(days)
+	{
+		var d = new Date(); d.setDate(d.getDate() + days);
+
+		v += '; expires=' + d.toUTCString();
+	}
+
+	if(path) v += '; path=' + enc(path);
+
+	document.cookie = enc(name) + '=' + v; // + '; Secure'
+};//___________________________________________________________________________
+
