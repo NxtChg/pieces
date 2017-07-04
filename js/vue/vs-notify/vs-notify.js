cb75ddb9cd847a93c557202adb009ba453b8266b
NxtChg <admin@nxtchg.com>
2017-07-04 18:32:06 +0300

    Styles moved out.

--- js/vue/vs-notify/vs-notify.js
+++ js/vue/vs-notify/vs-notify.js
@@ -1,4 +1,25 @@
 
+var s = document.createElement("style"); s.type = 'text/css';
+
+document.getElementsByTagName('head')[0].appendChild(s);
+
+s.innerHTML =
+		'.vs-notify{ position:fixed; width:300px; z-index:9999; }'+
+		'.ntf{ font-size:14px; padding:10px; margin:0 5px 5px; color:#fff; background:#44A4FC; border-left:5px solid #187FE7; box-sizing:border-box; text-align:left; cursor:pointer; }'+
+		'.warn   { background:#ffb648; border-left-color:#f48a06; }'+
+		'.error  { background:#E54D42; border-left-color:#B82E24; }'+
+		'.success{ background:#68CD86; border-left-color:#42A85F; }'+
+
+		'.ntf-left-enter-active, .ntf-left-leave-active, .ntf-right-enter-active, .ntf-right-leave-active, .ntf-top-enter-active, .ntf-top-leave-active,'+
+		'.ntf-bottom-enter-active, .ntf-bottom-leave-active{ transition: all 0.3s; }'+
+		'.ntf-left-enter,  .ntf-left-leave-to { opacity:0; transform:translateX(-300px); }'+
+		'.ntf-right-enter, .ntf-right-leave-to{ opacity:0; transform:translateX(300px); }'+
+		'.ntf-fade-enter-active, .ntf-fade-leave-active{ transition: opacity 0.5s; }'+
+		'.ntf-fade-enter, .ntf-fade-leave-to{ opacity: 0; }'+
+		'.ntf-top-enter,  .ntf-top-leave-to{ opacity:0; transform: translateY(-120px); }'+
+		'.ntf-bottom-enter, .ntf-bottom-leave-to{ opacity:0; transform: translateY(120px); }';
+//_____________________________________________________________________________
+
 var VsNotify =
 {
  install: function(Vue)
@@ -23,22 +44,6 @@ Vue.component('vs-notify',
 				'</slot>'+
 			'</div>'+
 		'</transition-group></div>',
-
-	style:
-		'.vs-notify{ position:fixed; width:300px; z-index:9999; }'+
-		'.ntf{ font-size:14px; padding:10px; margin:0 5px 5px; color:#fff; background:#44A4FC; border-left:5px solid #187FE7; box-sizing:border-box; text-align:left; cursor:pointer; }'+
-		'.warn   { background:#ffb648; border-left-color:#f48a06; }'+
-		'.error  { background:#E54D42; border-left-color:#B82E24; }'+
-		'.success{ background:#68CD86; border-left-color:#42A85F; }'+
-
-		'.ntf-left-enter-active, .ntf-left-leave-active, .ntf-right-enter-active, .ntf-right-leave-active, .ntf-top-enter-active, .ntf-top-leave-active,'+
-		'.ntf-bottom-enter-active, .ntf-bottom-leave-active{ transition: all 0.3s; }'+
-		'.ntf-left-enter,  .ntf-left-leave-to { opacity:0; transform:translateX(-300px); }'+
-		'.ntf-right-enter, .ntf-right-leave-to{ opacity:0; transform:translateX(300px); }'+
-		'.ntf-fade-enter-active, .ntf-fade-leave-active{ transition: opacity 0.5s; }'+
-		'.ntf-fade-enter, .ntf-fade-leave-to{ opacity: 0; }'+
-		'.ntf-top-enter,  .ntf-top-leave-to{ opacity:0; transform: translateY(-120px); }'+
-		'.ntf-bottom-enter, .ntf-bottom-leave-to{ opacity:0; transform: translateY(120px); }',
 
 	props:
 	{
