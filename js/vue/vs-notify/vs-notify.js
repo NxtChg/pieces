7b569a33cc89a8321cdcd5027d2b9ed874b1e374
NxtChg <admin@nxtchg.com>
2017-07-04 18:33:11 +0300

    Minor fix.

--- js/vue/vs-notify/vs-notify.js
+++ js/vue/vs-notify/vs-notify.js
@@ -5,10 +5,10 @@ document.getElementsByTagName('head')[0].appendChild(s);
 
 s.innerHTML =
 		'.vs-notify{ position:fixed; width:300px; z-index:9999; }'+
-		'.ntf{ font-size:14px; padding:10px; margin:0 5px 5px; color:#fff; background:#44A4FC; border-left:5px solid #187FE7; box-sizing:border-box; text-align:left; cursor:pointer; }'+
-		'.warn   { background:#ffb648; border-left-color:#f48a06; }'+
-		'.error  { background:#E54D42; border-left-color:#B82E24; }'+
-		'.success{ background:#68CD86; border-left-color:#42A85F; }'+
+		'.vs-notify .ntf{ font-size:14px; padding:10px; margin:0 5px 5px; color:#fff; background:#44A4FC; border-left:5px solid #187FE7; box-sizing:border-box; text-align:left; cursor:pointer; }'+
+		'.vs-notify .warn   { background:#ffb648; border-left-color:#f48a06; }'+
+		'.vs-notify .error  { background:#E54D42; border-left-color:#B82E24; }'+
+		'.vs-notify .success{ background:#68CD86; border-left-color:#42A85F; }'+
 
 		'.ntf-left-enter-active, .ntf-left-leave-active, .ntf-right-enter-active, .ntf-right-leave-active, .ntf-top-enter-active, .ntf-top-leave-active,'+
 		'.ntf-bottom-enter-active, .ntf-bottom-leave-active{ transition: all 0.3s; }'+
